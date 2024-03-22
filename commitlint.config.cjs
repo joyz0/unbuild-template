6bf3461dd3e0059a15725bc1ad37c172bf6ee1b7
zhuzy <1214327383@qq.com>
2024-03-22 10:43:25 +0800

    lint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   // 'build', 'ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'
   extends: ['@commitlint/config-conventional'],
 }
